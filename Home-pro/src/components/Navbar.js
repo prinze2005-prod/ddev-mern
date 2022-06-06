0a7fe9083a461a09faeb91a0e9319dfdf8db3314
Kayleen <gaoliu216@hotmail.com>
2022-06-06 11:41:00 -0600

    update new logo in sidebar

--- Home-pro/src/components/Navbar.js
+++ Home-pro/src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import logo from "../assets/logo.svg";
 import homepro from "../assets/homepro.jpg";
 import { FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
--- Home-pro/src/components/Sidebar.js
+++ Home-pro/src/components/Sidebar.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import logo from '../assets/logo.svg'
-import { Link } from 'react-router-dom'
-import { useProductsContext } from '../context/products_context'
-import { FaTimes } from 'react-icons/fa'
-import { links } from '../utils/constants'
-import styled from 'styled-components'
-import CartButtons from './CartButtons'
-import { useUserContext } from '../context/user_context'
+import React from "react";
+import homepro from "../assets/homepro.jpg";
+import { Link } from "react-router-dom";
+import { useProductsContext } from "../context/products_context";
+import { FaTimes } from "react-icons/fa";
+import { links } from "../utils/constants";
+import styled from "styled-components";
+import CartButtons from "./CartButtons";
+import { useUserContext } from "../context/user_context";
 
 const Sidebar = () => {
-  const { isSidebarOpen, closeSidebar } = useProductsContext()
-  const { myUser } = useUserContext()
+  const { isSidebarOpen, closeSidebar } = useProductsContext();
+  const { myUser } = useUserContext();
 
   return (
     <SidebarContainer>
       <aside
-        className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}
+        className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}
       >
-        <div className='sidebar-header'>
-          <img src={logo} className='logo' alt='comfy sloth' />
-          <button className='close-btn' type='button' onClick={closeSidebar}>
+        <div className="sidebar-header">
+          <img src={homepro} className="logo" alt="comfy sloth" />
+          <button className="close-btn" type="button" onClick={closeSidebar}>
             <FaTimes />
           </button>
         </div>
-        <ul className='links'>
+        <ul className="links">
           {links.map(({ id, text, url }) => {
             return (
               <li key={id}>
@@ -31,11 +31,11 @@ const Sidebar = () => {
                   {text}
                 </Link>
               </li>
-            )
+            );
           })}
           {myUser && (
             <li>
-              <Link to='/checkout' onClick={closeSidebar}>
+              <Link to="/checkout" onClick={closeSidebar}>
                 checkout
               </Link>
             </li>
@@ -44,8 +44,8 @@ const Sidebar = () => {
         <CartButtons />
       </aside>
     </SidebarContainer>
-  )
-}
+  );
+};
 
 const SidebarContainer = styled.div`
   text-align: center;
@@ -116,6 +116,6 @@ const SidebarContainer = styled.div`
       display: none;
     }
   }
-`
+`;
 
-export default Sidebar
+export default Sidebar;

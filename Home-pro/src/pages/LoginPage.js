b53cdfdfab492c40f58c2c0425a712a81e387c3a
Kayleen <gaoliu216@hotmail.com>
2022-06-30 13:43:48 -0600

    Update LoginPage.js
    update form

--- Home-pro/src/pages/LoginPage.js
+++ Home-pro/src/pages/LoginPage.js
@@ -4,18 +4,52 @@ import {
   MDBCard,
   MDBCardBody,
   MDBCardTitle,
-  MDBInput,
   MDBCol,
   MDBRow,
-  MDBBtn,
 } from "mdb-react-ui-kit";
 import Container from "react-bootstrap/Container";
 import { PageHero } from "../components";
+import { useRef } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
 
 function LoginPage() {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  let message = "";
+
+  async function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    const loginData = {
+      email: enteredEmail,
+      password: enteredPassword,
+    };
+    //will not throw error if server sends back error code (404, etc...)
+    try {
+      const response = await fetch("http://localhost:5000/account/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: loginData.email,
+          password: loginData.password,
+        }),
+      });
+      message = await response.json();
+      console.log(message);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <main>
-      <PageHero title="Login" />
       <Container>
         <br></br>
         <br></br>
@@ -27,28 +61,30 @@ function LoginPage() {
 
               <center>
                 <form>
-                  <MDBInput
-                    className="mb-4"
-                    type="email"
-                    id="form2Example1"
-                    label="Email address"
-                  />
-                  <MDBInput
-                    className="mb-4"
-                    type="password"
-                    id="form2Example2"
-                    label="Password"
-                  />
-
-                  <MDBBtn
-                    type="submit"
-                    className="mb-4"
-                    color="warning"
-                    block
-                    style={{ color: "black" }}
+                  <FloatingLabel
+                    controlId="floatingEmailInputGrid"
+                    label="Email"
                   >
-                    Sign in
-                  </MDBBtn>
+                    <Form.Control
+                      type="email"
+                      placeholder="Email"
+                      ref={emailInputRef}
+                    />
+                  </FloatingLabel>
+
+                  <br></br>
+                  <FloatingLabel
+                    controlId="floatingPasswordInputGrid"
+                    label="Password"
+                  >
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      ref={passwordInputRef}
+                    />
+                  </FloatingLabel>
+                  <br></br>
+
                   <MDBRow className="mb-4">
                     <MDBCol>
                       <Link to="#">Forgot password?</Link>
@@ -58,7 +94,16 @@ function LoginPage() {
                         Not a member? <Link to="/signup">Register</Link>
                       </p>
                     </MDBCol>
+                    <span>{message}</span>
                   </MDBRow>
+                  <Button
+                    variant="warning"
+                    type="submit"
+                    onClick={submitHandler}
+                    style={{ color: "black" }}
+                  >
+                    Submit
+                  </Button>
                 </form>
               </center>
             </MDBCardBody>

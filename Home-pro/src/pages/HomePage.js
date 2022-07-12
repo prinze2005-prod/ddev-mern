08ac9d740bb11563c5d0bea59db0dbcac2148e68
Saksham9132 <saksham9132@gmail.com>
2022-07-11 18:40:50 -0600

    List of previous services for logged in user

--- Home-pro/src/pages/HomePage.js
+++ Home-pro/src/pages/HomePage.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { FeaturedProducts, Hero, Services, Contact } from '../components'
-
-const HomePage = () => {
+import ListOfServices from '../components/ListOfServices'
+import { useUserContext } from "../context/user_context";
+const HomePage = ({user}) => {
+  const { myUser } = useUserContext();
   return (
     <main>
       <Hero />
       <FeaturedProducts />
+      {user && (
+         <ListOfServices/>
+      )}
       
+
     </main>
   )
 }

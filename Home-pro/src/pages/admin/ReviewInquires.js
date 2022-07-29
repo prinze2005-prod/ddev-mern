f73d85db8f7c09210e276edebc19da164c9c6533
Saksham9132 <saksham9132@gmail.com>
2022-07-28 22:42:42 -0600

    Update ReviewInquires.js
    uses array of object inquiries and map it

--- Home-pro/src/pages/admin/ReviewInquires.js
+++ Home-pro/src/pages/admin/ReviewInquires.js
@@ -6,6 +6,19 @@ import Table from "react-bootstrap/Table";
 import { Link } from "react-router-dom";
 
 function ReviewInquires() {
+    const inquiryData =[
+        {
+           email: 'joeblow@gmail.com',
+           serviceNumber: 125568,
+           inquiry: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'
+        },
+        {
+            email: 'jakeblow@gmail.com',
+            serviceNumber: 124778,
+            inquiry: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'
+        }
+    ];
+
   return (
     <main>
       <TechHeader title="Review Inquires" />
@@ -23,16 +36,17 @@ function ReviewInquires() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>joeblow@gmail.com</td>
-              <td>154655</td>
-              <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</td>
-            </tr>
-            <tr>
-              <td>joeblow@gmail.com</td>
-              <td>14545545454554</td>
-              <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</td>
-            </tr>
+                {inquiryData.map(obj =>
+                    <tr>
+
+                     <td>{obj.email} </td>
+                     <td>{obj.serviceNumber}</td>
+                     <td>{obj.inquiry}</td>
+                     </tr>
+                 )}
+              
+            
+            
           </tbody>
         </Table>
         <center>

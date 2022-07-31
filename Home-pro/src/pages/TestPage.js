d6515f08f83414a2a235df69ac9698ac549f2be9
NLScotty <Scott.B.Normore@gmail.com>
2022-07-31 15:20:46 -0600

    Take notes on this Page
    @Saksham9132
    @kayleenliu
    
    How to fetch information from server and display it on the page.

--- Home-pro/src/pages/TestPage.js
+++ Home-pro/src/pages/TestPage.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { Button, Form } from "react-bootstrap";
 /*
 import Container from "react-bootstrap/Container";
@@ -11,6 +11,61 @@ import Select from 'react-select';
 */
 
 function TestPage() {
+  let HP_refreshToken;
+  let HP_accessToken;
+  try {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+      var cook = cookies[i].split("=");
+      if (cook[0].includes("HP_refreshToken")) {
+        HP_refreshToken = cook[1];
+      }
+      if (cook[0].includes("HP_accessToken")) {
+        HP_accessToken = cook[1];
+      }
+    }
+  }catch(err){
+    console.log(err)
+  }
+  const [profileData, setProfileData] = useState([,,]);
+  useEffect(() => {
+    fetch("http://localhost:5000/api/customer/getLoggedInInfo",
+    {
+      method: "POST",
+      credentials: "include", //TWO THINGS: Cookies and this header <============
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        refreshToken: HP_refreshToken, // <==================== IN ALL REQUESTS THAT ARE CUSTOMER, TECH, and EMAIL!
+        accessToken: HP_accessToken, // <====================== IN ALL REQUESTS THAT ARE CUSTOMER, TECH, and EMAIL!
+      })})
+    .then(response => response.json())
+    .then(data => setProfileData([data.fName, data.lName,data.phoneNumber]));
+  },[]);
+
+  return (
+    <main>
+      <p>
+        {profileData[0]} 
+        {profileData[1]}
+        {profileData[2]}
+      </p>
+    </main>
+  );
+}
+
+export default TestPage;
+
+
+
+  /*
+
+
+          <Button variant="warning" style={{ color: "black" }} onClick={submitHandler}>
+            Test Button
+        </Button>{" "}
+
   async function submitHandler(event) {
     event.preventDefault();
     //will not throw error if server sends back error code (404, etc...)
@@ -46,14 +101,4 @@ function TestPage() {
       console.log(err);
     }
   }
-
-  return (
-    <main>
-        <Button variant="warning" style={{ color: "black" }} onClick={submitHandler}>
-            Test Button
-        </Button>{" "}
-    </main>
-  );
-}
-
-export default TestPage;
+  */

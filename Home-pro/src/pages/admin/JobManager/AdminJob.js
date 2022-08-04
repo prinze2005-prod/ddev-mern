ae6dfd79ed921962b60dda3948eefc33f0172225
NLScotty <Scott.B.Normore@gmail.com>
2022-08-03 19:19:39 -0600

    Update AdminJob.js
    Update Admin Job List

--- Home-pro/src/pages/admin/JobManager/AdminJob.js
+++ Home-pro/src/pages/admin/JobManager/AdminJob.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Table from "react-bootstrap/Table";
-import { Link } from "react-router-dom";
+import { Link, useHistory} from "react-router-dom";
+const { REACT_APP_API_ENDPOINT } = process.env;
 
 function AdminJob() {
-  const jobData = [
+  let history = useHistory();
+
+  let HP_refreshToken;
+  let HP_accessToken;
+
+  const [jobData, setJobData] = useState([
     {
       jobId: 12345,
       tech_email: "info@calgaryplumbing.com",
@@ -28,14 +34,59 @@ function AdminJob() {
       address: [{ street: "13 Sait way NW", postalCode: "A1A 1B1" }],
       service_id: 1,
     },
-  ];
+  ]);
+
+  function checkUnassigned(job){
+    return job.status==='unassigned';
+  }
+
+  function checkAssigned(job){
+    return job.status==='assigned';
+  }
+  let unassignedData = jobData.filter(checkUnassigned);
+  let assignedData = jobData.filter(checkAssigned);
+  try {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+      var cook = cookies[i].split("=");
+      if (cook[0].includes("HP_refreshToken")) {
+        HP_refreshToken = cook[1];
+      }
+      if (cook[0].includes("HP_accessToken")) {
+        HP_accessToken = cook[1];
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+  useEffect(() => {
+    fetch(REACT_APP_API_ENDPOINT +"5000/api/admin/getjobs", {
+      method: "POST",
+      credentials: "include", //TWO THINGS: Cookies and this header <============
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        refreshToken: HP_refreshToken, // <==================== IN ALL REQUESTS THAT ARE CUSTOMER, TECH, and EMAIL!
+        accessToken: HP_accessToken, // <====================== IN ALL REQUESTS THAT ARE CUSTOMER, TECH, and EMAIL!
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => setJobData(data))
+      .then(
+
+
+
+      );
+  }, []);
 
   return (
     <main>
       <Container>
         <br></br>
         <center>
-          <h2>List of Jobs</h2>
+          <h2>List of Unassigned Jobs</h2>
         </center>
 
         <br></br>
@@ -56,25 +107,17 @@ function AdminJob() {
             </tr>
           </thead>
           <tbody>
-            {jobData.map((obj) => (
+            {unassignedData.map((obj) => (
               <tr>
-                <td>{obj.jobId} </td>
+                <td>{obj.job_id} </td>
                 <td>{obj.tech_email} </td>
                 <td>{obj.first_name}</td>
                 <td>{obj.last_name}</td>
                 <td>{obj.cust_email} </td>
                 <td>{obj.status} </td>
                 <td>{obj.start_time} </td>
-                <td>
-                  {obj.address.map((add) => (
-                    <div> {add.street} </div>
-                  ))}{" "}
-                </td>
-                <td>
-                  {obj.address.map((add) => (
-                    <div> {add.postalCode} </div>
-                  ))}{" "}
-                </td>
+                <td>{obj.address.street} </td>
+                <td>{obj.address.postalCode} </td>
                 <td>{obj.service_id} </td>
                 <td>
                   {" "}
@@ -91,6 +134,58 @@ function AdminJob() {
           </tbody>
         </Table>
         <br></br>
+
+        <br></br>
+        <center>
+          <h2>List of Assigned Jobs</h2>
+        </center>
+
+        <br></br>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Job ID</th>
+              <th>Technician Email</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Customer Email</th>
+              <th>Status</th>
+              <th>Start Time</th>
+              <th>Street</th>
+              <th>Postal Code</th>
+              <th>Service Id</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody>
+            {assignedData.map((obj) => (
+              <tr>
+                <td>{obj.job_id} </td>
+                <td>{obj.tech_email} </td>
+                <td>{obj.first_name}</td>
+                <td>{obj.last_name}</td>
+                <td>{obj.cust_email} </td>
+                <td>{obj.status} </td>
+                <td>{obj.start_time} </td>
+                <td>{obj.address.street} </td>
+                <td>{obj.address.postalCode} </td>
+                <td>{obj.service_id} </td>
+                <td>
+                  {" "}
+                  <Link
+                    to="/adminjobedit"
+                    className="btn"
+                    style={{ backgroundColor: "#ffb347" }}
+                  >
+                    Edit job
+                  </Link>{" "}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <br></br>
+
         <center>
           <Link
             to="/admin"
@@ -100,6 +195,8 @@ function AdminJob() {
             back to Admin page
           </Link>
         </center>
+        <br></br>
+        <br></br>
       </Container>
     </main>
   );

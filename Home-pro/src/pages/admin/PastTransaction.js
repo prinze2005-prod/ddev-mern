f2d7e82ec8150005b70703aa67a22c85efa6b961
Saksham9132 <saksham9132@gmail.com>
2022-08-03 22:25:46 -0600

    Created/fixed PastTransaction page

--- Home-pro/src/pages/admin/PastTransaction.js
+++ Home-pro/src/pages/admin/PastTransaction.js
@@ -1,216 +1,106 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import { Container, Table } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory, Link } from 'react-router-dom';
 
 const PastTransaction = () => {
+	let history = useHistory();
+
+	let HP_refreshToken;
+	let HP_accessToken;
+  
+	const [transactionData, setTransactionData] = useState([
+	  {
+		job_id: 12,
+		serviceName: "Electrical",
+		duration: 3,
+		first_2_hour_charge: 150,
+		additional_per_hour: 80,
+		total_charge: 230
+	  },
+	  {
+		job_id: 14,
+		serviceName: "Plumbing",
+		duration: 2.5,
+		first_2_hour_charge: 150,
+		additional_per_hour: 80,
+		total_charge: 190
+	  },
+	]);
+  
+	try {
+	  var cookies = document.cookie.split(";");
+	  for (var i = 0; i < cookies.length; i++) {
+		var cook = cookies[i].split("=");
+		if (cook[0].includes("HP_refreshToken")) {
+		  HP_refreshToken = cook[1];
+		}
+		if (cook[0].includes("HP_accessToken")) {
+		  HP_accessToken = cook[1];
+		}
+	  }
+	} catch (err) {
+	  console.log(err);
+	}
+  
+  
 	return (
 		<main>
-			<PaintTitle style={{ padding: '0.25rem' }}>
-				<h2>Past Transaction</h2>
-			</PaintTitle>
-			<br />
-			<SearchForm>
-				<label>Order ID</label>
-				<input type="text" />
-
-				<label>Start</label>
-				<input type="date" />
-
-				<label>End</label>
-				<input type="date" />
-
-				<EditButton
-					type="submit"
-					className="btn btn-outline"
-					style={{ color: 'black', background: '#ffb347' }}
-				>
-					Search
-				</EditButton>
-			</SearchForm>
+			<Container style={{ minHeight: "500px" }}>
+        <br></br>
+				<center><h2>Past Transaction</h2></center>
+		
 			<br />
 			<br />
-			<Wrapper className="page section section-center ">
-				<article>
-					<div className="title"></div>
+			<Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>Job ID</th>
+              <th>Service Name</th>
+              <th>Duration</th>
+              <th>First Two Hour Charge</th>
+			  <th>Additional Per Hour</th>
+              <th>Total Charge</th>
+              <th>Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactionData.map((obj) => (
+              <tr>
+                <td>{obj.job_id} </td>
+                <td>{obj.serviceName} </td>
+                <td>{obj.duration} </td>
+                <td>{obj.first_2_hour_charge} </td>
+				<td>{obj.additional_per_hour}</td>
+                <td>{obj.total_charge} </td>
+				<td>
+				{" "}
+				<Link
+            to="/pasttransactiondetail"
+            className="btn"
+            style={{ backgroundColor: "#ffb347" }}
+          >
+           Details
+          </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <center>
+          <Link
+            to="/admin"
+            className="btn"
+            style={{ backgroundColor: "#ffb347" }}
+          >
+            back to Admin page
+          </Link>
+          <br></br>
+          <br></br>
+        </center>
+      </Container>
+    </main>
+  );
+}
 
-					<Container>
-						<Table responsive striped hover size="sm" className="noWrap">
-							<thead>
-								<tr>
-									<th>Order ID</th>
-									<th>Date</th>
-									<th>Date</th>
-									<th>Details</th>
-
-									<th></th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>1......6</td>
-									<td>March 23, 2022</td>
-									<td>$1000</td>
-
-									<td>
-										<EditButton
-											className="btn btn-outline"
-											style={{ color: 'black', background: '#ffb347' }}
-										>
-											Details
-										</EditButton>
-									</td>
-								</tr>
-								<tr>
-									<td>1......6</td>
-									<td>March 23, 2022</td>
-									<td>$1000</td>
-
-									<td>
-										<EditButton
-											className="btn btn-outline"
-											style={{ color: 'black', background: '#ffb347' }}
-										>
-											Details
-										</EditButton>
-									</td>
-								</tr>
-								<tr>
-									<td>1......6</td>
-									<td>March 23, 2022</td>
-									<td>$1000</td>
-
-									<td>
-										<EditButton
-											className="btn btn-outline"
-											style={{ color: 'black', background: '#ffb347' }}
-										>
-											Details
-										</EditButton>
-									</td>
-								</tr>
-								<tr>
-									<td>1......6</td>
-									<td>March 23, 2022</td>
-									<td>$1000</td>
-
-									<td>
-										<EditButton
-											className="btn btn-outline"
-											style={{ color: 'black', background: '#ffb347' }}
-										>
-											Details
-										</EditButton>
-									</td>
-								</tr>
-								<tr>
-									<td>1......6</td>
-									<td>March 23, 2022</td>
-									<td>$1000</td>
-
-									<td>
-										<EditButton
-											className="btn btn-outline"
-											style={{ color: 'black', background: '#ffb347' }}
-										>
-											Details
-										</EditButton>
-									</td>
-								</tr>
-							</tbody>
-						</Table>
-					</Container>
-
-					<DefaultButton
-						className="btn btn-outline"
-						style={{ color: 'black', background: '#ffb347' }}
-					>
-						<NavLink
-							to="/admin"
-							style={(isActive) => ({
-								color: isActive ? '#ffb347' : 'black',
-							})}
-						>
-							<center>Home</center>
-						</NavLink>
-					</DefaultButton>
-				</article>
-			</Wrapper>
-		</main>
-	);
-};
-
-const Wrapper = styled.section`
-	text-align: center;
-	padding: 0.25rem;
-	height: 40rem;
-	margin-top: -0.05rem;
-
-	td,
-	th,
-	tr {
-		border: none;
-	}
-	table {
-		width: 100%;
-	}
-`;
-
-const PaintTitle = styled.div`
-	background: var(--clr-primary-5);
-	display: flex;
-	justify-content: center;
-	text-align: center;
-`;
-
-const DeleteButton = styled.button`
-	background: var(--clr-primary-5);
-	border: none;
-	border-radius: 0.25rem;
-	text-transform: capitalize;
-	padding: 0.25rem;
-	width: 150px;
-	float: right;
-`;
-const DefaultButton = styled.button`
-	background: var(--clr-primary-5);
-	color: #fff;
-	border: none;
-	border-radius: 0.25rem;
-	cursor: pointer;
-	text-transform: capitalize;
-	padding: 0.25rem;
-	display: block;
-	width: 150px;
-	margin: 1rem left;
-`;
-
-const EditButton = styled.button`
-	background: var(--clr-primary-5);
-	color: #fff;
-	border: none;
-	border-radius: 0.25rem;
-	text-transform: capitalize;
-	padding: 0.25rem;
-	width: 80px;
-`;
-
-const BackButton = styled.button`
-	background: var(--clr-primary-5);
-	color: #fff;
-	border: none;
-	border-radius: 0.25rem;
-	text-transform: capitalize;
-	padding: 0.25rem;
-	width: 110px;
-	float: right;
-`;
-
-const SearchForm = styled.div`
-	width: 50rem;
-	float: center;
-	align-items: center;
-	margin-left: 40rem;
-	box-sizing: 10px;
-`;
 export default PastTransaction;
